4e5575951ee18fadd852ddfe77707a815747499c
Harsh <sainiharsh22@gmail.com>
2021-01-31 16:35:17 +0530

    webhook changes

--- echo_webbot.py
+++ echo_webbot.py
@@ -2,6 +2,7 @@ import logging
 from flask import Flask, request
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters,Dispatcher
 from telegram import Bot,Update
+
 # enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
@@ -23,7 +24,7 @@ def index():
 # view to handle webhooks
 @app.route(f'/{TOKEN}', methods=['GET', 'POST'])
 def webhook():
-   # """webhook view which receives updates from telegram"""
+   # webhook view which receives updates from telegram
     # create update object from json-format request data
     update = Update.de_json(request.get_json(), bot)
     # process update
@@ -34,29 +35,29 @@ def webhook():
 
 # By default any function pass inside command handler it will get to arguments
 def start(bot,update):
-   # """callback function for /start handler"""
+   # callback function for /start handler
     print(update)
     author = update.message.from_user.first_name # who has send me /sart command
     reply = "Hi! {}".format(author) # reply hi to author
     bot.send_message(chat_id=update.message.chat_id,text=reply) # send reply text to bot
 
 def _help(bot,update):
-   # """callback function for /help handler"""
+   # callback function for /help handler
     help_txt="Hey! This is a help text."
     bot.send_message(chat_id=update.message.chat_id,text=help_txt)
 
 def echo_text(bot,update):
-   # """callback function for text message handler"""
+   # callback function for text message handler
     reply = update.message.text
     bot.send_message(chat_id=update.message.chat_id,text=reply)
 
 def echo_sticker(bot,update):
-   # """callback function for sticker message handler"""
+   # callback function for sticker message handler
     bot.send_sticker(chat_id=update.message.chat_id,
     sticker=update.message.sticker.file_unique_id)
 
 def error(bot,update):
-   # """callback function for error handler"""
+   # callback function for error handler
     logger.error("Update '%s' caused error '%s'", update, update.error) 
  
 # creating entry point for my program
